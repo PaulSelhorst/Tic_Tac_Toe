753ec7937da3a5aa238d37a6a05ec268561c782e
bestbuddy135 <jpoentis@pauoaes.net>
2016-03-24 14:08:46 -1000

    TicTacToe Buttons
    yeah

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function squares(){
+	document.getElementById('square1').innerHTML ="<button id='button1'>1</button>";
+	document.getElementById('square2').innerHTML ="<button id='button1'>2</button>";
+	document.getElementById('square3').innerHTML ="<button id='button1'>3</button>";
+	document.getElementById('square4').innerHTML ="<button id='button1'>4</button>";
+	document.getElementById('square5').innerHTML ="<button id='button1'>5</button>";
+	document.getElementById('square6').innerHTML ="<button id='button1'>6</button>";
+	document.getElementById('square7').innerHTML ="<button id='button1'>7</button>";
+	document.getElementById('square8').innerHTML ="<button id='button1'>8</button>";
+	document.getElementById('square9').innerHTML ="<button id='button1'>9</button>";
+}
